ed2e019f22091cbc3fe37c03daf186d5259636b9
maxwen <max.weninger@gmail.com>
2015-03-29 18:39:55 +0200

    [1/2] base: allow disable of screenshot shutter sound
    Change-Id: I47d52bba21170118af87d35376d81d7569587a2f
    
    # Conflicts:
    #	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3731,6 +3731,13 @@ public final class Settings {
         };
 
         /**
+         *Screenshot sound enable, This is the noise made when taking a screenshot
+         * Defaults to 1 - sounds enabled
+         * @hide
+         */
+        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
